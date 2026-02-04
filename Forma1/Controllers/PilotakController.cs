98a85f478fc6eb9bdd7c974830678d05c63bbda8
Luczi Alex <luczialex@gmail.com>
2026-02-04 09:22:29 +0100

    Pilota lekerdezese nev alapjan

--- Forma1/Controllers/PilotakController.cs
+++ Forma1/Controllers/PilotakController.cs
@@ -12,7 +12,6 @@ namespace Forma1.Controllers
     public class PilotakController : ControllerBase
     {
         [HttpGet]
-
         public IActionResult GetPilotak()
         {
             using (var cx = new Forma1Context())
@@ -31,7 +30,6 @@ namespace Forma1.Controllers
         }
 
         [HttpGet("{pazon}")]
-
         public IActionResult GetPilota(int pazon)
         {
             try
@@ -45,6 +43,48 @@ namespace Forma1.Controllers
             catch (Exception ex)
             {
                 return StatusCode(401, ex.InnerException.Message);
+            }
+        }
+
+        [HttpGet("pnev")]
+        public async Task<IActionResult> GetPilotaByNev(string pnev)
+        {
+            try
+            {
+                using (var cx = new Forma1Context())
+                {
+                    var pilota = await cx.Pilotaks
+                        .Where(p => p.Pnev == pnev)
+                        .Include(p => p.CsapatNavigation)
+                        .Include(p => p.Eredmenyeks)
+                        .Select(p => new
+                        {
+                            Pazon = p.Pazon,
+                            Pnev = p.Pnev,
+                            Szev = p.Szev,
+                            Csapat = p.CsapatNavigation == null ? null : new
+                            {
+                                Csazon = p.CsapatNavigation.Csazon,
+                                Csnev = p.CsapatNavigation.Csnev
+                            },
+                            Eredmenyeks = p.Eredmenyeks.Select(e => new
+                            {
+                                Nagydij = e.Nagydij,
+                                Startpoz = e.Startpoz,
+                                Celpoz = e.Celpoz
+                            }).ToList()
+                        })
+                        .FirstOrDefaultAsync();
+
+                    if (pilota == null)
+                        return NotFound();
+
+                    return Ok(pilota);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
             }
         }
     }
