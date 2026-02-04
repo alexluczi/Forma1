935e30e09ccb98b58adbbef745c8eec39f2ea7bb
Luczi Alex <luczialex@gmail.com>
2026-02-04 08:54:12 +0100

    Osszes pilota lekerdezese

--- Forma1/Controllers/PilotakController.cs
+++ Forma1/Controllers/PilotakController.cs
@@ -1,6 +1,33 @@
-﻿namespace Forma1.Controllers
+﻿using Forma1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Crypto.Digests;
+using System.Reflection;
+
+namespace Forma1.Controllers
 {
-    public class PilotakController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PilotakController : ControllerBase
     {
+        [HttpGet]
+
+        public IActionResult GetPilotak()
+        {
+            using (var cx = new Forma1Context())
+            {
+                try
+                {
+                    var pilotak = cx.Pilotaks.ToList();
+                    return Ok(pilotak);
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(408, ex.Message);
+                    return StatusCode(StatusCodes.Status408RequestTimeout, ex.Message);
+                }
+            }
+        }
     }
 }
